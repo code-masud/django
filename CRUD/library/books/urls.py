c0839f69f4fc2cb9f50f510ffc9f2f9f95b2ae81
code-masud <code.masud@hotmail.com>
2026-01-14 12:49:28 +0600

    commit

--- CRUD/library/books/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.BookListView.as_view(), name='book-list'),
-    path('<int:pk>/book/', views.BookDetailView.as_view(), name='book-detail'),
-    path('/add/book/', views.BookCreateView.as_view(), name='book-create'),
-    path('/edit/book/<int:pk>/', views.BookUpdateView.as_view(), name='book-update'),
-    path('/delete/book/<int:pk>/', views.BookDeleteView.as_view(), name='book-delete'),
-]
